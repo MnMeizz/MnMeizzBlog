4277030ac5ce75615862247735f2720a3edaa330
MnMeizz <1059809116@qq.com>
2026-01-23 14:04:26 +0800

    修复评论区大陆区域无法访问

--- src/config.ts
+++ src/config.ts
@@ -393,7 +393,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://asd-blush-sigma.vercel.app/",
+		envId: "https://mnmei-twikoo.hf.space",
 		lang: "zh-CN",
 	},
 };
