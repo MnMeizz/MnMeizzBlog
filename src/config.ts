8e4444fb3bdec17a65f038fe8fa8f90bd6a9da27
MnMeizz <1059809116@qq.com>
2026-01-28 20:03:28 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "MnMeizz",
 	subtitle: "Blog",
-	siteURL: "http://124.221.162.52/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://mnmeizz.online", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-01-18", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
@@ -393,7 +393,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://mnmei-twikoo.hf.space",
+		envId: "https://mnmei-twikooplugins.hf.space",
 		lang: "zh-CN",
 	},
 };
